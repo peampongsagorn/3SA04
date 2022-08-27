66a53f4f2ef24ea7a9fd97f4ae788da07156ab61
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2022-08-27 18:25:48 +0700

    3

--- Wish.js
+++ Wish.js
@@ -1,5 +1,12 @@
 import React from "react";
 export default
 class Wish extends React.Component{
-    
+    constructor(props){
+        super(props);
+        this.state = {
+          bwish: "Click here if you have bad day "
+        }
+      }
+  
+      
   }
