667c4e0c4caa548e3b8c449c805aed4e9e647380
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2022-08-27 15:59:32 +0700

    Connect

--- components/Weather.js
+++ components/Weather.js
@@ -8,6 +8,25 @@ export default function Weather(props) {
         description: '-',
         temp: 0
     })
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=736e69f417e5c7e2a3f229bc61af8977`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+
+    
 
     return (
         <View>
