d04e48f3d0889e0afc2dcab0941b8cc4f12ab43c
Pongsagorn Juntasuwan <pongsagorn0910@gmail.com>
2022-08-27 15:41:08 +0700

    Create Forecast

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet,  View } from 'react-native';
 import Weather from './components/Weather';
+import Constant from 'expo-constants';
 export default function App() {
   
   return (
@@ -15,8 +16,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constant.statusBarHeight
   },
 });
